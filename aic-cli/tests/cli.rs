c6c1c56225f85633e05dc20e0e0c1be8e8f11d9c
Dylan Stark <dylan.stark@gmail.com>
2025-05-02 19:22:14 -0500

    Adds test with cached JSON.

--- aic-cli/tests/cli.rs
+++ aic-cli/tests/cli.rs
@@ -1,20 +1,16 @@
 use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
 use predicates::prelude::predicate;
 use std::process::Command;
 
 #[test]
-fn artworks_resource() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("aic-cli")?;
-    cmd.arg("artworks");
-
-    cmd.assert().success();
-    Ok(())
-}
-
-#[test]
 fn listing_artworks_outputs_json() -> Result<(), Box<dyn std::error::Error>> {
+    let cached_artworks_file = assert_fs::NamedTempFile::new("artworks.json")?;
+    cached_artworks_file.write_str("{}")?;
+
     let mut cmd = Command::cargo_bin("aic-cli")?;
-    cmd.arg("artworks");
+    cmd.env("AIC_CACHE_DIR", cached_artworks_file.parent().unwrap())
+        .arg("artworks");
 
     let stdout = String::from_utf8(cmd.output()?.stdout)?;
 
