0364ac6ba79c88c4c4798e18ff975168744af3bf
Dylan Stark <dylan.stark@gmail.com>
2025-05-04 09:00:17 -0500

    Adds basic docs for aic-cli.

--- aic-cli/src/main.rs
+++ aic-cli/src/main.rs
@@ -1,15 +1,26 @@
+#![warn(missing_docs)]
+
+//! aic-cli is a simple CLI for accessing the Art Institute of Chicago's [public APIs].
+//!
+//! You can get the (first page of the) artworks listing with `aic-cli artworks`.
+//!
+//! [public APIs]: https://api.artic.edu/docs/#introduction
+
 use clap::Parser;
 
+#[doc(hidden)]
 #[derive(Clone, clap::ValueEnum)]
 enum Resource {
     Artworks,
 }
 
+#[doc(hidden)]
 #[derive(Parser)]
 struct Cli {
     resource: Resource,
 }
 
+#[doc(hidden)]
 #[tokio::main]
 async fn main() {
     let args = Cli::parse();
