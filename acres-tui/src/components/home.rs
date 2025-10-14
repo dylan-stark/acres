9f73d59d00cac69ff5e0382baea73222594486ee
Dylan Stark <dylan.stark@gmail.com>
2025-10-14 16:11:01 -0500

    Sets up Home component to just log actions.

--- acres-tui/src/components/home.rs
+++ acres-tui/src/components/home.rs
@@ -8,17 +8,13 @@ use crate::action::Action;
 #[derive(Default)]
 pub struct Home {
     action_tx: Option<UnboundedSender<Action>>,
-    text: Text<'static>,
+    log: Vec<String>,
+    messages_width: u16,
 }
 
 impl Home {
     pub fn new() -> Self {
         Self::default()
-    }
-
-    fn set_text(&mut self) -> Result<()> {
-        self.text = Text::from("Hi!");
-        Ok(())
     }
 }
 
@@ -29,39 +25,37 @@ impl Component for Home {
     }
 
     fn init(&mut self, _area: Size) -> Result<()> {
-        self.text = Text::from("Hi!");
         Ok(())
     }
 
     fn update(&mut self, action: Action) -> Result<Option<Action>> {
-        #[allow(clippy::single_match)]
         match action {
-            Action::ToText => self.set_text()?,
-            _ => {}
+            Action::Tick => return Ok(None),
+            Action::Render => return Ok(None),
+            _ => {
+                let message = format!("action: {}", action);
+                self.messages_width = self.messages_width.max(message.len() as u16);
+                self.log.push(message);
+            }
         }
         Ok(None)
     }
 
-    /// Draws the ASCII image (`text`) centered in the viewing area.
-    ///
-    /// This is called either while Rendering or Resizing. In the case
-    /// of Resizing, it is called after the TUI is resized but before
-    /// the component receives the Resize action.
     fn draw(&mut self, frame: &mut Frame, area: Rect) -> Result<()> {
-        let [_, middle, _] = Layout::horizontal([
-            Constraint::Fill(1),
-            Constraint::Min(self.text.width() as u16),
-            Constraint::Fill(1),
-        ])
-        .areas(area);
-        let [_, middle, _] = Layout::vertical([
-            Constraint::Fill(1),
-            Constraint::Min(self.text.height() as u16),
-            Constraint::Fill(1),
-        ])
-        .areas(middle);
-        let widget = Paragraph::new(self.text.clone());
-        frame.render_widget(widget, middle);
+        let [_, right] =
+            Layout::horizontal([Constraint::Fill(1), Constraint::Max(self.messages_width)])
+                .areas(area);
+
+        let messages = self
+            .log
+            .iter()
+            .rev()
+            .take(right.height.into())
+            .map(|m| m.to_string());
+        let messages = List::new(messages)
+            .direction(ListDirection::BottomToTop)
+            .style(Style::new().light_green().on_dark_gray().italic());
+        frame.render_widget(messages, right);
         Ok(())
     }
 }
