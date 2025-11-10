776c56e4158f790d9e2f4b2e65f398b263a29903
Dylan Stark <dylan.stark@gmail.com>
2025-11-10 08:06:48 -0600

    Adds example using builder.

--- acres/src/lib.rs
+++ acres/src/lib.rs
@@ -41,7 +41,6 @@
 //! #     }
 //! # }
 //! # );
-//!
 //! # let mock_server = wiremock::MockServer::start().await;
 //! # let mock_uri = format!("{}/api/v1", mock_server.uri());
 //! # wiremock::Mock::given(wiremock::matchers::any())
@@ -50,7 +49,6 @@
 //! #     .expect(1)
 //! #     .mount(&mock_server)
 //! #     .await;
-//!
 //! let api = acres::Api::new();
 //! # let api = acres::Api::builder().base_uri(&mock_uri).use_cache(false).build();
 //!
@@ -60,13 +58,81 @@
 //! # }
 //! ```
 //!
-//! We provide [builders] for most everything. Where possible, sensible defaults are used so that
-//! you only need provide specific details for your use case.
+//! We provide [builders] for endpoints with more options. Where possible, sensible defaults are used so that
+//! you only need provide specific details for your use case. For example, you can [list artworks]
+//! while limiting the number of results and choosing which fields to return.
+//!
+//! ```rust
+//! # use serde_json::json;
+//! # use anyhow::Result;
+//! use acres::artworks::request::artworks;
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<()> {
+//! # let body = json!(
+//! # {
+//! #    "pagination": {
+//! #        "total": 116941,
+//! #        "limit": 2,
+//! #        "offset": 0,
+//! #        "total_pages": 58471,
+//! #        "current_page": 1,
+//! #        "next_url": "https://api.artic.edu/api/v1/artworks?page=2&limit=2"
+//! #    },
+//! #    "data": [
+//! #        {
+//! #            "id": 14556,
+//! #            "api_link": "https://api.artic.edu/api/v1/artworks/14556",
+//! #            "title": "Auvers, Panoramic View",
+//! #        },
+//! #        {
+//! #            "id": 8961,
+//! #            "api_link": "https://api.artic.edu/api/v1/artworks/8961",
+//! #            "title": "Head of Arthur Jerome Eddy",
+//! #        }
+//! #    ],
+//! #    "info": {
+//! #        "license_text": "The `description` field in this response is licensed under a Creative Commons Attribution 4.0 Generic License (CC-By) and the Terms and Conditions of artic.edu. All other data in this response is licensed under a Creative Commons Zero (CC0) 1.0 designation and the Terms and Conditions of artic.edu.",
+//! #        "license_links": [
+//! #            "https://creativecommons.org/publicdomain/zero/1.0/",
+//! #            "https://www.artic.edu/terms"
+//! #        ],
+//! #        "version": "1.13"
+//! #    },
+//! #    "config": {
+//! #        "iiif_url": "https://www.artic.edu/iiif/2",
+//! #        "website_url": "https://www.artic.edu"
+//! #    }
+//! # }
+//! # );
+//! # let mock_server = wiremock::MockServer::start().await;
+//! # let mock_uri = format!("{}/api/v1", mock_server.uri());
+//! # wiremock::Mock::given(wiremock::matchers::any())
+//! #     .and(wiremock::matchers::path("/api/v1/artworks"))
+//! #     .and(wiremock::matchers::query_param("limit", "2"))
+//! #     .and(wiremock::matchers::query_param("fields", "id,title,api_link"))
+//! #     .respond_with(wiremock::ResponseTemplate::new(200).set_body_json(body))
+//! #     .expect(1)
+//! #     .mount(&mock_server)
+//! #     .await;
+//! let api = acres::Api::new();
+//! # let api = acres::Api::builder().base_uri(&mock_uri).use_cache(false).build();
+//!
+//! let request = artworks::Request::builder()
+//!     .base_uri(api.base_uri())
+//!     .limit(Some(2))
+//!     .fields(Some(vec!["id".into(), "title".into(), "api_link".into()]))
+//!     .build()?;
+//! let json: acres::Cached = api.fetch(request.to_string()).await?;
+//! # Ok(())
+//! # }
+//! ```
 //!
 //! [Artworks collection]: https://api.artic.edu/docs/#artworks
 //! [API helper]: struct.Api.html
 //! [fetch function]: fn.fetch.html
 //! [get artwork by id]: https://api.artic.edu/docs/#get-artworks-id
+//! [list artworks]: https://api.artic.edu/docs/#get-artworks
 //! [public APIs]: https://api.artic.edu/docs/#introduction
 //! [`acres-cli`]: ../acres_cli/index.html
 //! [`acres-tui`]: ../acres_tui/index.html
